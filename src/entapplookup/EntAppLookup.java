6e914bf5fa54328a9ea20361b5478c71c1e957a8
Thufir <hawat.thufir@gmail.com>
2014-10-11 20:58:47 -0700

    initial commit

---
+++ src/entapplookup/EntAppLookup.java
@@ -0,0 +1,20 @@
+package entapplookup;
+
+import ejb.EJBRemoteInterface;
+import java.util.logging.Logger;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class EntAppLookup {
+
+    private static final Logger log = Logger.getLogger(EntAppLookup.class.getName());
+
+    public static void main(String... args) throws NamingException {
+        Context ctx = new InitialContext();
+        log.info(ctx.getEnvironment().toString());
+        Object obj = ctx.lookup("ejb.EJBRemoteInterface");
+        EJBRemoteInterface ejb = (EJBRemoteInterface) obj;
+        log.info(ejb.getResult());
+    }
+}
